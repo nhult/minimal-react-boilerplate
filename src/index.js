08aef15b59a07f4846d1ad3b0705ad7e974b1c11
nhult <n.hulttinen97@gmail.com>
2018-06-30 13:51:40 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Minimal React Boilerplate</h1>,
+  document.getElementById('root')
+);
+
+module.hot.accept();
